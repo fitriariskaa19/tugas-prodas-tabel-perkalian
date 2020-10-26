e2e83f3079eb19e75c7725443ddad2c5cae542d2
fitriariskaa19 <72713210+fitriariskaa19@users.noreply.github.com>
2020-10-26 19:14:07 +0700

    Add files via upload

---
+++ tugas prodas no.6.cpp
@@ -0,0 +1,26 @@
+/* Program Tabel Perkalian */
+/* Fitri Ariska */
+/* 20051397082 */
+/* 2020B */
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+main(){
+int i,j;
+cout<<"Tabel Perkalian \n";
+
+
+for(i=1;i<=10;i++)
+{cout<<" "<<i;
+
+
+for(j=1;j<=10;j++)
+cout<<" "<<i*j;
+cout<<endl;
+}
+
+getch();
+
+}
